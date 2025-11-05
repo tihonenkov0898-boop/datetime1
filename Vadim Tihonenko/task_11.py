4cbe4d19da2b9db0cf2e921f0d5b2a2f17759717
Vadim24Isip <tihonenkov0898@gmail.com>
2025-11-05 08:46:08 +0500

    Update task_11.py

--- Vadim Tihonenko/task_11.py
+++ Vadim Tihonenko/task_11.py
@@ -1,12 +1,1 @@
-from datetime import date, timedelta
-def count_working_days(start, end):
-    days = 0
-    current = start
-    while current <= end:
-        if current.weekday() < 5:
-            days += 1
-        current += timedelta(days=1)
-    return days
-start_date = date(2024, 1, 1)
-end_date = date(2024, 1, 31)
-print(f"{count_working_days(start_date, end_date)} рабочих дней (исключая выходные)")  
+
